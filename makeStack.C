d8efe868f2dd49be7b08b86f142fe66736959cb5
dsklein <dsklein@physics.ucsd.edu>
2015-11-06 13:15:10 -0800

    Simplify 'makeStack' by getting sample names from the analysis object

--- makeStack.C
+++ makeStack.C
@@ -27,6 +27,7 @@ void makeStack( analysis* myAnalysis) {
   varNames.push_back("dphilw" );  axisLabels.push_back("#Delta#phi");
   varNames.push_back("bkgtype");  axisLabels.push_back("Background type");
   varNames.push_back("njets"  );  axisLabels.push_back("Number of jets");
+  varNames.push_back("nbtags" );  axisLabels.push_back("Number of b-tags");
   
   vector<TString> regNames; //signal regions
   regNames.push_back("low250");
@@ -43,72 +44,25 @@ void makeStack( analysis* myAnalysis) {
   for( uint j=0; j<regNames.size(); j++ ) {
 	for( uint i=0; i<varNames.size(); i++ ) {
 
-	  TString name_tt2l		= varNames.at(i) + "_tt2l_"     + regNames.at(j);
-	  TString name_tt1l		= varNames.at(i) + "_tt1l_"     + regNames.at(j);
-	  // TString name_wb		= varNames.at(i) + "_Wb_"       + regNames.at(j);
-	  // TString name_wlight	= varNames.at(i) + "_Wucsd_"    + regNames.at(j);
-	  TString name_wjets    = varNames.at(i) + "_wjets_"    + regNames.at(j);
-	  TString name_dy		= varNames.at(i) + "_dy_"       + regNames.at(j);
-	  // TString name_stst		= varNames.at(i) + "_STstchan_" + regNames.at(j);
-	  // TString name_sttw		= varNames.at(i) + "_STtWchan_" + regNames.at(j);
-	  TString name_singletop= varNames.at(i) + "_singletop_" + regNames.at(j);
-	  // TString name_ttw		= varNames.at(i) + "_ttw_"      + regNames.at(j);
-	  // TString name_ttz		= varNames.at(i) + "_ttz_"      + regNames.at(j);
-	  // TString name_vv		= varNames.at(i) + "_vv_"       + regNames.at(j);
-	  // TString Name_tzq       = varNames.at(i) + "_tzq_"      + regNames.at(j);
-	  TString name_rare		= varNames.at(i) + "_rare_"     + regNames.at(j);
+	  vector<TH1F*> bkgs;
+	  vector<TH1F*> sigs;
 
-	  TString name_stop850	= varNames.at(i) + "_stop850_"  + regNames.at(j);
-	  TString name_stop650	= varNames.at(i) + "_stop650_"  + regNames.at(j);
-	  TString name_stop500	= varNames.at(i) + "_stop500_"  + regNames.at(j);
-	  TString name_stop425	= varNames.at(i) + "_stop425_"  + regNames.at(j);
+	  // Retrieve the histograms for each background
+	  for( TString sampleName : myAnalysis->GetBkgNamesStorage() ) {
+		TString plotname = varNames.at(i) + "_" + sampleName + "_" + regNames.at(j);
+		TH1F*   histo    = (TH1F*)plotfile->Get(plotname);
+		bkgs.push_back(histo);
+	  }
 
-	  // Extract a histogram for each of the backgrounds and signals
-	  TH1F* h_tt2l		= (TH1F*)plotfile->Get(name_tt2l);
-	  TH1F* h_tt1l		= (TH1F*)plotfile->Get(name_tt1l);
-	  // TH1F* h_wb		= (TH1F*)plotfile->Get(name_wb);
-	  // TH1F* h_wlight	= (TH1F*)plotfile->Get(name_wlight);
-	  TH1F* h_wjets 	= (TH1F*)plotfile->Get(name_wjets);
-	  TH1F* h_dy		= (TH1F*)plotfile->Get(name_dy);
-	  // TH1F* h_stst		= (TH1F*)plotfile->Get(name_stst);
-	  // TH1F* h_sttw		= (TH1F*)plotfile->Get(name_sttw);
-	  TH1F* h_singletop	= (TH1F*)plotfile->Get(name_singletop);
-	  // TH1F* h_ttw		= (TH1F*)plotfile->Get(name_ttw);
-	  // TH1F* h_ttz		= (TH1F*)plotfile->Get(name_ttz);
-	  // TH1F* h_vv		= (TH1F*)plotfile->Get(name_vv);
-	  // TH1F* h_tzq    = (TH1F*)plotfile->Get(name_tzq);
-	  TH1F* h_rare		= (TH1F*)plotfile->Get(name_rare);
+	  // Retrieve the histograms for each signal
+	  for( TString sampleName : myAnalysis->GetSignalNamesStorage() ) {
+		TString plotname = varNames.at(i) + "_" + sampleName + "_" + regNames.at(j);
+		TH1F*   histo    = (TH1F*)plotfile->Get(plotname);
+		sigs.push_back(histo);
+	  }
 
-
-	  TH1F* h_stop850	= (TH1F*)plotfile->Get(name_stop850);
-	  TH1F* h_stop650	= (TH1F*)plotfile->Get(name_stop650);
-	  TH1F* h_stop500	= (TH1F*)plotfile->Get(name_stop500);
-	  TH1F* h_stop425	= (TH1F*)plotfile->Get(name_stop425);
 
 	  TH1F* nullData = new TH1F("", "", 1, 0, 1);
-
-	  // String histos into vectors for Alex's plotmaking code
-	  vector<TH1F*> bkgs;
-	  bkgs.push_back(h_tt2l);
-	  bkgs.push_back(h_tt1l);
-	  // bkgs.push_back(h_wb);
-	  // bkgs.push_back(h_wlight);
-	  bkgs.push_back(h_wjets);
-	  bkgs.push_back(h_dy);
-	  // bkgs.push_back(h_stst);
-	  // bkgs.push_back(h_sttw);
-	  bkgs.push_back(h_singletop);
-	  // bkgs.push_back(h_ttw);
-	  // bkgs.push_back(h_ttz);
-	  // bkgs.push_back(h_vv);
-	  // bkgs.push_back(h_tzq);
-	  bkgs.push_back(h_rare);
-
-	  vector<TH1F*> sigs;
-	  sigs.push_back(h_stop850);
-	  sigs.push_back(h_stop650);
-	  sigs.push_back(h_stop500);
-	  sigs.push_back(h_stop425);
 
 	  // Get sample titles and colors from the "analysis" object
 	  vector<string> bkg_titles = myAnalysis->GetBkgNamesLegend();
