f2ffef7f847f0b0dc928b50240e6eb3c6cbebf87
dsklein <dsklein@physics.ucsd.edu>
2016-10-03 16:50:21 -0700

    Rewrite datacard maker to remove a bunch of redundant calculations

--- makeDataCards.cc
+++ makeDataCards.cc
@@ -11,7 +11,6 @@
 using namespace std;
 
 
-
 void makeDataCards( analysis* myAnalysis ) {
 
   // Do the basic setup stuff
@@ -60,17 +59,158 @@ void makeDataCards( analysis* myAnalysis ) {
 	// Had a LONG discussion with John and FKW about why they want us to do it this way
 	h_sigYield->Add( h_sigContam, -1. );
 
-	// Get bin sizes
+	// Get bin sizes for the signal yield histogram
 	double binwidthX = h_sigYield->GetXaxis()->GetBinWidth(1);
 	double binwidthY = h_sigYield->GetYaxis()->GetBinWidth(1);
 
-	// Loop over signal mass points
+
+	///////////////////////////////////////////////////////////////////////////////////////
+	// Begin constructing a template datacard for this particular signal region.
+
+	vector<TString> cardlines, uncertlines;
+	TString tmpstr;
+
+
+	// Top matter
+
+	cardlines.push_back( Form( "# Data card for signal region %d (%s)\n", reg, sigRegions.at(reg-1).Data() ) );
+	cardlines.push_back( "### Placeholder line. SUSY masses will go here." );
+	cardlines.push_back("---\n");
+	cardlines.push_back("imax 1  number of channels\n");
+
+	tmpstr = Form( "jmax %d  number of backgrounds (", nBkgs );
+	for (TString bkgName : bkgs ) tmpstr += Form( "%s, ",  bkgName.Data() );   // Will usually be 4 (znunu, 2l, 1ltop, 1lw)
+	tmpstr += ")\n";
+	cardlines.push_back( tmpstr );
+	cardlines.push_back( "### Placeholder line. Number of uncertainties will go here.\n" );
+	cardlines.push_back(  "---\n" );
+
+	cardlines.push_back( "# Now list the number of events observed (or zero if no data)\n" );
+	cardlines.push_back( Form("bin %d\n", reg) );
+	if( myAnalysis->HasData() ) cardlines.push_back( Form( "observation %d\n", int(h_bkgYield->GetBinContent(1)) ) );
+	else                        cardlines.push_back(       "observation 0\n" );
+	cardlines.push_back("---\n");
+
+	cardlines.push_back(  "# Now list the expected events (i.e. Monte Carlo yield) for signal and all backgrounds in our particular bin\n" );
+	cardlines.push_back(  "# Second 'process' line should be zero for signal, and a positive integer for each background\n" );
+
+	tmpstr = "bin      ";
+	for( TString sample : samples ) tmpstr += Form( " %10i", reg );  // Write the signal region number once for each sample (signal and bkg)
+	tmpstr += "\n";
+	cardlines.push_back( tmpstr );
+
+	tmpstr = "process  ";
+	for( TString sample : samples ) tmpstr += Form( " %10s", sample.Data() );  // Write the name of each sample (sig & bkg)
+	tmpstr += "\n";
+	cardlines.push_back( tmpstr );
+
+	tmpstr = "process  ";
+	for( int j=0; j<nSamples; j++ ) tmpstr += Form( " %10i", j ); // Write a number for each sample (0=signal, positive integers for bkgs)
+	tmpstr += "\n";
+	cardlines.push_back( tmpstr );
+
+
+	// Precalculate the yields for each background process
+
+	double yield;
+	TString bkgRates;
+
+	for( int i=1; i<nSamples; i++ ) {
+	  if( i==2 ) yield = h_lostLep->GetBinContent(reg); // Pull 2l yield from lostLepton estimate histogram
+	  else       yield = h_bkgYield->GetBinContent(i+2);
+	  bkgRates += Form( " %10f", yield );
+	}
+	bkgRates += "\n";
+	cardlines.push_back( "### Placeholder line. Observed rate of background processes will go here." );
+
+	cardlines.push_back( "---\n" );
+	cardlines.push_back( "# Now we list the independent sources of uncertainty (syst. and stat. error), and which samples they affect\n" );
+	cardlines.push_back( "---\n" );
+
+
+	/////////////////////////////////////////////////////////////////////////
+	// Make a similar template for the rows that store all the uncertainties
+
+	// Generate a row for the statistical uncertainty on each sample (except signal, which is handled later)
+	uncertlines.push_back( "### Placeholder line. Stat uncertainty on signal yield will go here." );
+	for( int sampleIdx=1; sampleIdx<nSamples; sampleIdx++ ) {
+
+	  char statname[25];
+	  sprintf( statname, "Stat%s%d", samples.at(sampleIdx).Data(), reg );
+	  tmpstr = Form( "%-18s  lnN ", statname );
+
+	  double statErr = 1.0 + (h_bkgYield->GetBinError(sampleIdx+2) / h_bkgYield->GetBinContent(sampleIdx+2) );
+	  if( sampleIdx==2 ) statErr = 1.0 + ( h_lostLep->GetBinError(reg) / h_lostLep->GetBinContent(reg) );  // Pull 2l stat error from lostLepton estimate histogram
+
+	  for( int j=0; j<nSamples; j++ ) {
+		if( j == sampleIdx )  tmpstr += Form( "  %8.6f  ", statErr);
+		else tmpstr += "     -      ";
+	  }
+	  tmpstr += "\n";
+	  uncertlines.push_back( tmpstr );
+	}
+
+
+	// Write out a dummy systematic uncertainty for each of the backgrounds that doesn't have an actual systematic calculation
+	for( int sampleIdx = 1; sampleIdx<nSamples; sampleIdx++ ) { // For now, skip the signal sample (don't give it a systematic uncertainty)
+
+	  if( sampleIdx == 2 && nVars > 0 ) continue;
+
+	  char systname[25];
+	  sprintf( systname, "Flat%s", samples.at(sampleIdx).Data() );
+	  tmpstr = Form( "%-18s  lnN ", systname );
+
+	  double systErr = 1.3; // Flat 30% systematic for now
+
+	  for( int j=0; j<nSamples; j++ ) {
+		if( j == sampleIdx )  tmpstr += Form( "  %8.6f  ", systErr);
+		else tmpstr +=  "     -      " ;
+	  }
+	  tmpstr += "\n";
+	  uncertlines.push_back( tmpstr );
+	}
+
+
+	// Write out all the systematic variations on the dilepton background
+	if( nVars > 0 ) {
+	  double nominal = h_lostLep->GetBinContent(reg);
+
+	  for( auto& iter : systMap ) {  // Loop over all distinct systematics
+
+		tmpstr = Form( "Syst%-14s  lnN ", iter.first.Data() );
+
+		// Loop over all variations of this systematic, and find the biggest difference from nominal
+		double maxdiff = 0.;
+		for( TString varName : iter.second ) {
+		  TH1D* h_tmp = (TH1D*)lostlepFile->Get( "variation_" + varName );
+		  maxdiff = max( maxdiff, fabs(nominal - h_tmp->GetBinContent(reg)) );
+		}
+
+		// Write the rest of the systematic row
+		for( int j=0; j<nSamples; j++ ) {
+		  if( j == 2 )  tmpstr += Form( "  %8.6f  ", 1.0 + maxdiff/nominal );
+		  else tmpstr += "     -      " ;
+		}
+		tmpstr += "\n";
+		uncertlines.push_back( tmpstr );
+	  }
+	}
+
+	// Count number of uncertainties, and insert appropriate row into datacard template
+	int nUncerts = uncertlines.size();
+	cardlines.at(5) = Form( "kmax %d  number of uncertainties\n", nUncerts );
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+	// Loop over signal mass points, to get the information that's specific to each mass point
+
 	for( int xbin=1; xbin<=h_sigYield->GetNbinsX(); xbin++ ) {
 	  for( int ybin=1; ybin<=h_sigYield->GetNbinsY(); ybin++ ) {
 
 		// Round bin centers to sensible numbers (nearest integer multiple of the bin width)
-		int stopmass = int(binwidthX) * int( round( h_sigYield->GetXaxis()->GetBinCenter(xbin) / binwidthX ) );
-		int lspmass  = int(binwidthY) * int( round( h_sigYield->GetYaxis()->GetBinCenter(ybin) / binwidthY ) );
+		int stopmass = binwidthX * round( h_sigYield->GetXaxis()->GetBinCenter(xbin) / binwidthX );
+		int lspmass  = binwidthY * round( h_sigYield->GetYaxis()->GetBinCenter(ybin) / binwidthY );
 
 		double sigYield = h_sigYield->GetBinContent( xbin, ybin );
 		double sigError = h_sigYield->GetBinError( xbin, ybin );
@@ -82,137 +222,41 @@ void makeDataCards( analysis* myAnalysis ) {
 		  sigError = 0.00000000000001;
 		}
 
+		// Generate the second line of the datacard, with the susy masses
+		cardlines.at(1) = Form( "# Stop mass = %d, LSP mass = %d\n", stopmass, lspmass );
 
-		// Do some acrobatics to send the output to a file...
+		// Generate line 17, with the "rate" for signal and backgrounds
+		tmpstr = "rate     ";
+		tmpstr += Form( " %10f", sigYield );
+		tmpstr += bkgRates;
+		cardlines.at(16) = tmpstr;
+
+		// Generate the line that holds the stat uncertainty on signal
+		char statname[25];
+		sprintf( statname, "Stat%s%d", samples.at(0).Data(), reg );
+		tmpstr = Form( "%-18s  lnN ", statname );
+
+		double statErr = 1.0 + ( sigError / sigYield );
+		tmpstr += Form( "  %8.6f  ", statErr);
+		for( int j=1; j<nSamples; j++ ) tmpstr += "     -      ";
+		tmpstr += "\n";
+		uncertlines.at(0) = tmpstr;
+
+
+		///////////////////////////////////////////////////////////////////
+		// Now let's actually make a datacard!
+
 		TString fileName = Form( "datacards/datacard_%s_T2tt_%d_%d.txt", sigRegions.at(reg-1).Data(), stopmass, lspmass );
 
+		// Open file
 		FILE * outfile;
 		outfile = fopen( fileName.Data(), "w" );
 
+		// Write out each line
+		for( TString line : cardlines   ) fputs( line.Data(), outfile );
+		for( TString line : uncertlines ) fputs( line.Data(), outfile );
 
-		////////////////////////////
-		// Now print the data card
-
-		fprintf( outfile,  "# Data card for signal region %d (%s)\n", reg, sigRegions.at(reg-1).Data() );
-		fprintf( outfile,  "# Stop mass = %d, LSP mass = %d\n", stopmass, lspmass );
-		fprintf( outfile,  "---\n" );
-		fprintf( outfile,  "imax 1  number of channels\n" );
-
-		fprintf( outfile,  "jmax %d  number of backgrounds (", nBkgs );
-		for (TString bkgName : bkgs ) fprintf( outfile, "%s, ",  bkgName.Data() );   // Will usually be 4 (znunu, 2l, 1ltop, 1lw)
-		fprintf( outfile,  ")\n" );
-
-		int nUncerts = 2*nSamples - 1;
-		if( nVars > 0 ) nUncerts += systMap.size() -1;  // Statistical uncertainty for each sample, and dummy or real systematics for each bkg
-		fprintf( outfile,  "kmax %d  number of uncertainties\n", nUncerts );
-		fprintf( outfile,  "---\n" );
-
-		fprintf( outfile,  "# Now list the number of events observed (or zero if no data)\n" );
-		fprintf( outfile,  "bin %d\n", reg );
-		if( myAnalysis->HasData() ) fprintf( outfile,  "observation %d\n", int(h_bkgYield->GetBinContent(1)) );
-		else                        fprintf( outfile,  "observation 0\n" );
-		fprintf( outfile,  "---\n" );
-
-		fprintf( outfile,  "# Now list the expected events (i.e. Monte Carlo yield) for signal and all backgrounds in our particular bin\n" );
-		fprintf( outfile,  "# Second 'process' line should be zero for signal, and a positive integer for each background\n" );
-
-		fprintf( outfile,  "bin      ");
-		for( TString sample : samples ) fprintf( outfile,  " %10i", reg );  // Print the signal region number once for each sample (signal and bkg)
-		fprintf( outfile, "\n" );
-
-		fprintf( outfile,  "process  ");
-		for( TString sample : samples ) fprintf( outfile,  " %10s", sample.Data() );  // Print the name of each sample (sig & bkg)
-		fprintf( outfile, "\n" );
-
-		fprintf( outfile,  "process  ");
-		for( int j=0; j<nSamples; j++ ) fprintf( outfile,  " %10i", j ); // Print a number for each sample (0=signal, positive integers for bkgs)
-		fprintf( outfile, "\n" );
-
-
-
-		// Print row with the yields for each process (signal, bkgs)
-		fprintf( outfile,  "rate     ");
-		double yield;
-
-		for( int i=1; i<=nSamples; i++ ) {
-		  if(      i==1 ) yield = sigYield;
-		  else if( i==3 ) yield = h_lostLep->GetBinContent(reg); // Pull 2l yield from lostLepton estimate histogram
-		  else            yield = h_bkgYield->GetBinContent(i+1);
-		  fprintf( outfile,  " %10f", yield );
-		}
-		fprintf( outfile, "\n" );
-
-		fprintf( outfile,  "---\n" );
-		fprintf( outfile,  "# Now we list the independent sources of uncertainty (syst. and stat. error), and which samples they affect\n" );
-		fprintf( outfile,  "---\n" );
-
-
-
-		// Print out a row for the statistical uncertainty on each sample
-		for( int sampleIdx=0; sampleIdx<nSamples; sampleIdx++ ) {
-
-		  char statname[25];
-		  sprintf( statname, "Stat%s%d", samples.at(sampleIdx).Data(), reg );
-		  fprintf( outfile,   "%-18s  lnN ", statname );
-
-		  double statErr = 1.0 + (h_bkgYield->GetBinError(sampleIdx+2) / h_bkgYield->GetBinContent(sampleIdx+2) );
-		  if( sampleIdx==0 ) statErr = 1.0 + ( sigError / sigYield ); // Pull stat error on signal from special signal yield histogram
-		  if( sampleIdx==2 ) statErr = 1.0 + ( h_lostLep->GetBinError(reg) / h_lostLep->GetBinContent(reg) );  // Pull 2l stat error from lostLepton estimate histogram
-
-		  for( int j=0; j<nSamples; j++ ) {
-			if( j == sampleIdx )  fprintf( outfile, "  %8.6f  ", statErr);
-			else fprintf( outfile,  "     -      " );
-		  }
-		  fprintf( outfile, "\n" );
-		}
-
-
-		// Print out a dummy systematic uncertainty for each background (except possibly dilepton, if we're using actual systematic variations)
-		for( int sampleIdx = 1; sampleIdx<nSamples; sampleIdx++ ) { // For now, skip the signal sample (don't give it a systematic uncertainty)
-
-		  if( sampleIdx == 2 && nVars > 0 ) continue;
-
-		  char systname[25];
-		  sprintf( systname, "Flat%s", samples.at(sampleIdx).Data() );
-		  fprintf( outfile,  "%-18s  lnN ", systname );
-
-		  double systErr = 1.3; // Flat 30% systematic for now
-
-		  for( int j=0; j<nSamples; j++ ) {
-			if( j == sampleIdx )  fprintf( outfile, "  %8.6f  ", systErr);
-			else fprintf( outfile,  "     -      " );
-		  }
-		  fprintf( outfile, "\n" );
-		}
-
-
-		// Print out all the systematic variations on the dilepton background
-		if( nVars > 0 ) {
-		  double nominal = h_lostLep->GetBinContent(reg);
-
-		  for( auto& iter : systMap ) {  // Loop over all distinct systematics
-
-			fprintf( outfile,  "Syst%-14s  lnN ", iter.first.Data() );
-
-			// Loop over all variations of this systematic, and find the biggest difference from nominal
-			double maxdiff = 0.;
-			for( TString varName : iter.second ) {
-			  TH1D* h_tmp = (TH1D*)lostlepFile->Get( "variation_" + varName );
-			  maxdiff = max( maxdiff, fabs(nominal - h_tmp->GetBinContent(reg)) );
-			}
-
-			// Print the rest of the systematic row
-			for( int j=0; j<nSamples; j++ ) {
-			  if( j == 2 )  fprintf( outfile, "  %8.6f  ", 1.0 + maxdiff/nominal);
-			  else fprintf( outfile,  "     -      " );
-			}
-			fprintf( outfile, "\n" );
-
-		  }
-		}
-
-
-		// The end of this file.
+		// Close file
 		fprintf( outfile,  "---\n" );
 		fclose(outfile);
 
