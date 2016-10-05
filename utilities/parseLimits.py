973f6a0cd2ea335e37cd20bf48081c83110d5b17
dsklein <dsklein@physics.ucsd.edu>
2016-10-05 13:03:57 -0700

    Test new indentation settings

--- utilities/parseLimits.py
+++ utilities/parseLimits.py
@@ -8,23 +8,23 @@ h_limits_ichep = ROOT.TH2D("limits_ichep", "limits ichep",    37, 87.5, 1012.5, 
 
 
 for line in infile_corr.readlines():
-    fields = line.split()
-    limit  = float(fields[4])
-    filename = fields[0].split('_')
-    m_stop = float(filename[4]) # get the stop mass
-    m_lsp  = float(filename[5].split('.')[0]) # get the LSP mass
-    binnum = h_limits_corr.FindBin( m_stop, m_lsp )
-    h_limits_corr.SetBinContent( binnum, limit )
+	fields = line.split()
+	limit  = float(fields[4])
+	filename = fields[0].split('_')
+	m_stop = float(filename[4]) # get the stop mass
+	m_lsp  = float(filename[5].split('.')[0]) # get the LSP mass
+	binnum = h_limits_corr.FindBin( m_stop, m_lsp )
+	h_limits_corr.SetBinContent( binnum, limit )
 
 
 for line in infile_ichep.readlines():
-    fields = line.split()
-    limit  = float(fields[4])
-    filename = fields[0].split('_')
-    m_stop = float(filename[4]) # get the stop mass
-    m_lsp  = float(filename[5].split('.')[0]) # get the LSP mass
-    binnum = h_limits_ichep.FindBin( m_stop, m_lsp )
-    h_limits_ichep.SetBinContent( binnum, limit )
+	fields = line.split()
+	limit  = float(fields[4])
+	filename = fields[0].split('_')
+	m_stop = float(filename[4]) # get the stop mass
+	m_lsp  = float(filename[5].split('.')[0]) # get the LSP mass
+	binnum = h_limits_ichep.FindBin( m_stop, m_lsp )
+	h_limits_ichep.SetBinContent( binnum, limit )
 
 outfile = ROOT.TFile("limit_plots.root", "RECREATE")
 outfile.cd()
